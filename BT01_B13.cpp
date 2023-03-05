d032b4c772bf6c3f3326bae9a4793a4f71a9ad14
buiftheescoong <125168053+buiftheescoong@users.noreply.github.com>
2023-03-05 20:54:42 +0700

    up

---
+++ BT01_B13.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int x, y;
+    cin >> x >> y;
+
+    int distance = sqrt(x * x + y * y);
+
+    cout << distance;
+
+    return 0;
+
+}
